1611bd736c799ea096d91e62464ba8d0cf1f9e65
chloe azerty <chloe.rotaru@yahoo.com>
2022-07-25 15:10:05 +0200

    maj

--- src/main/java/com/wcs/doctors/Controller/DoctorController.java
+++ src/main/java/com/wcs/doctors/Controller/DoctorController.java
@@ -1,4 +1,4 @@
-package com.wildcodeschool.doctors.controller;
+package com.wcs.doctors.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
